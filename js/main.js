18bcf61c3f21b1e58bf148e76d9d5b009476ee06
KevAlegre <kovinalegre@gmail.com>
2023-11-27 17:09:37 -0300

    Master - Cambios en index

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 //El proyecto trata de un juego en donde se presentan operaciones matemáticas al usuario y al responder correctamente, suma un puntaje (siendo el límite 10) y a su vez subiendo al dificualtad. Al llegar al límite ganará pero si falla, se termina el juego y se indicará con un mensaje su puntaje. También se puede poner una variable en donde se guarde el record para que intente superarlo.
 
+let respuesta;
+let puntaje;
+let intentar;
+let dificultad = 25;
+
 function generarNumero() {
     let numUno = Math.ceil(Math.random()*dificultad);
     let numDos = Math.ceil(Math.random()*dificultad);
@@ -7,11 +12,6 @@ function generarNumero() {
     console.log("La operación a realizar es: " + numUno + "+" + numDos);
     return res;
 }
-
-let respuesta;
-let puntaje;
-let intentar;
-let dificultad = 25;
 
 console.log("----------------------=°=----------------------");
 console.log("La temática es fácil, deberás responder correctamente a las operaciones matemáticas. Si acertás sumas un punto y si fallás perdés.");
